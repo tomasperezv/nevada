0a361568d1b07ca2f71d6ff9e0cc9f8ebf81ca3b
Alejandro Rodriguez <hi@aro.io>
2016-06-22 18:04:39 +0200

    [FRONT-73] Added more modules to jQuery to extend compatibility with old browsers (#8)

--- src/javascript/dom/core.js
+++ src/javascript/dom/core.js
@@ -9,7 +9,9 @@ let $ = () => {};
 if (!process || process.env.NODE_ENV !== 'test') {
   $ = require('jquery/src/core');
   require('jquery/src/core/init');
+  require('jquery/src/core/parseHTML');
   require('jquery/src/css');
+  require('jquery/src/css/hiddenVisibleSelectors');
   require('jquery/src/attributes');
   require('jquery/src/data');
   require('jquery/src/dimensions');
@@ -17,6 +19,13 @@ if (!process || process.env.NODE_ENV !== 'test') {
   require('jquery/src/effects');
   require('jquery/src/deprecated');
   require('jquery/src/event');
+  require('jquery/src/event/alias');
+  require('jquery/src/ajax');
+  require('jquery/src/ajax/script');
+  require('jquery/src/ajax/xhr');
+  require('jquery/src/wrap');
+  require('jquery/src/serialize');
+  require('jquery/src/queue/delay');
 }
 
 module.exports = $;
