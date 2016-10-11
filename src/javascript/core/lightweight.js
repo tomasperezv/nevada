1e23cca21c247733ae67795b1dfad6980a42cfb2
Tomás Pérez <t@0x101.com>
2016-10-11 10:55:18 +0200

    Adding missing polyfills for internet explorer 11. (#48)

--- src/javascript/core/lightweight.js
+++ src/javascript/core/lightweight.js
@@ -7,4 +7,8 @@
 
 // Define the environment depending on NODE_ENV
 const Environment = require('../environment').default;
+require('core-js/modules/es6.date.now');
+require('core-js/modules/es6.symbol');
+require('core-js/es6/object');
+require('core-js/es6/map');
 Environment.version = Environment.NEVADA_LIGHTWEIGHT;
