b64e803488a795624677d87163f26b83a2ad1ad5
Tomás Pérez <t@0x101.com>
2016-11-28 14:20:19 +0100

    Customize uglifyjs parameters. (#76)
    We need to disable mangle to avoid problems in the generated bundles.

--- builder/plugin-initializer/uglify.js
+++ builder/plugin-initializer/uglify.js
@@ -11,8 +11,26 @@ const plugin = [];
 
 if (process.env.NODE_ENV === 'production') {
   plugin.push(new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      warnings: false
+    mangle: false,
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      sequences: true,
+      properties: true,
+      dead_code: true,
+      drop_debugger: true,
+      comparisons: true,
+      conditionals: true,
+      evaluate: true,
+      booleans: true,
+      loops: true,
+      unused: true,
+      hoist_funs: true,
+      if_return: true,
+      join_vars: true,
+      cascade: true,
+      negate_iife: true,
+      drop_console: true
     }
   }));
 }
